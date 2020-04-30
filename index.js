6db16cb6c361888e143032e892b4af346036393a
Ling Sang <lingsang.tse@mail.utoronto.ca>
2020-04-29 20:20:01 -0400

    return search with images and titles

--- index.js
+++ index.js
@@ -6,15 +6,28 @@ const fetchData = async (searchTerm) => {
         }
     });
 
-    console.log (response.data);
+    if (response.data.Error){
+
+    }
+    
+    return response.data.Search;
+
 };
 
 const input = document.querySelector ('input');
 
+const onInput =  async event => {
+    const movies = await fetchData (event.target.value);
+    for (let movie of movies){
+        const div = document.createElement ('div');
+        div.innerHTML =`
+        <img src="${movie.Poster}"/>
+        <h1>${movie.Title}</h1>
+        `;
 
-
-const onInput =  event => {
-    fetchData (event.target.value);
+        document.querySelector ('#target').appendChild (div)
+    }
+    
 };
 
 input.addEventListener ('input', debounce (onInput, 500));
